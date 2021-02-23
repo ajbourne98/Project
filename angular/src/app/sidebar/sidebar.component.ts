eee844f54e6900cfef3b5d9fa4c400a3eda5832f
Alex Bourne <52406314+Alex-Bourne@users.noreply.github.com>
2021-02-23 17:40:02 +0000

    more sidebar stuff

--- angular/src/app/sidebar/sidebar.component.ts
+++ angular/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,17 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  public faPlus = faPlus;
+  public faMinus = faMinus;
+
+  private sidebarItems: SidebarItem[];
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  public getSidebarItems(): String[] {
-    return [
-      'Home',
-      'Modules',
-      'My Stuff'
+    this.sidebarItems = [
+      {
+        label: 'Home',
+        subItems: null
+      },
+      {
+        label: 'Modules',
+        subItems: [
+          {
+            label: 'Module 1',
+            subItems: null
+          },
+          {
+            label: 'Module 2',
+            subItems: null
+          },
+          {
+            label: 'Module 3',
+            subItems: null
+          },
+          {
+            label: 'Module 4',
+            subItems: null
+          }
+        ],
+        expanded: false
+      },
+      {
+        label: 'My Stuff',
+        subItems: null
+      },
     ];
   }
+
+  public getSidebarItems(): SidebarItem[] {
+    return this.sidebarItems;
+  }
+
+  public onItemClick(item: SidebarItem): void {
+    item.expanded = !item.expanded;
+  }
+}
+
+export type SidebarItem = {
+  label: string;
+  subItems: SidebarItem[];
+  expanded?: boolean;
 }
