f74a465c35d308931516cb112c3316b69b44ba2d
Alex Bourne <52406314+Alex-Bourne@users.noreply.github.com>
2021-03-02 14:30:15 +0000

    can add categories and todo items

--- angular/src/app/activites/todo/todo.component.ts
+++ angular/src/app/activites/todo/todo.component.ts
@@ -10,7 +10,8 @@ export class TodoComponent implements OnInit {
   public showAddTodoItemModal = false;
   public addItemForm: FormGroup;
 
-  private tasks: todoItem[];
+  private tasks: TodoItem[];
+  private categories: Category[] = [];
 
   constructor(private fb: FormBuilder) { }
 
@@ -19,50 +20,39 @@ export class TodoComponent implements OnInit {
       {
         text: 'Test todo item 1',
         checked: false,
-        category: 'general'
+        category: 'GENERAL'
       },
       {
         text: 'Test todo item 2',
         checked: false,
-        category: 'module 1'
+        category: 'MODULE 1'
       },
       {
         text: 'Test todo item 3',
         checked: false,
-        category: 'general'
+        category: 'GENERAL'
       }
     ];
 
+    this.initCategories();
     this.initForm();
   }
 
-  public getTodoItems(): todoItem[] {
-    return this.tasks;
-  }
-
-  public getItemsByCategory(category: string): todoItem[] {
-    return this.tasks.filter((task: todoItem) => {
+  public getItemsByCategory(category: string): TodoItem[] {
+    return this.tasks.filter((task: TodoItem) => {
       return task.category === category;
     });
   }
 
-  public onItemCheck(task: todoItem): void {
+  public onItemCheck(task: TodoItem): void {
     task.checked = !task.checked;
   }
 
-  public getCategories(): string[] {
-    let a: string[] = [];
-
-    return this.tasks.map((task: todoItem) => {
-      return task.category;
-    }).filter((category: string) => {
-      let duplicate = a.includes(category);
-
-      if (!duplicate) {
-        a.push(category);
-      }
-
-      return !duplicate;
+  public getCategories(allowEmptyCategories: boolean): string[] {
+    return this.categories.filter((category: Category) => {
+      return allowEmptyCategories ? true : category.count > 0;
+    }).map((category: Category) => {
+      return category.name;
     });
   }
 
@@ -71,15 +61,80 @@ export class TodoComponent implements OnInit {
     this.showAddTodoItemModal = true;
   }
 
+  public addTodoItem(): void {
+    // Validate form
+    let category: string = this.addItemForm.value.category;
+    let text: string = this.addItemForm.value.text;
+
+    if (category.length < 1 || text.length < 1) {
+      return;
+    }
+
+    if (category == '-1') {
+      return;
+    }
+
+    // Create new todo item
+    this.tasks.push({
+      text,
+      checked: false,
+      category
+    });
+
+    this.initCategories();
+  }
+
+  public addNewCategory(): void {
+    // Validate form
+    let name: string = this.addItemForm.value.newCategory;
+
+    if (!name || (name && name.length < 1)) {
+      return;
+    }
+
+    this.categories.push({
+      name: name.toUpperCase(),
+      count: 0
+    });
+
+    this.addItemForm.controls.newCategory.reset();
+  }
+
   private initForm(): void {
     this.addItemForm = this.fb.group({
       text: [''],
+      category: [-1],
+      newCategory: ['']
+    });
+  }
+
+  private initCategories(): void {
+    this.tasks.map((task: TodoItem) => {
+      if (this.categories.some((category: Category) => {
+        return category.name === task.category;
+      })) {
+        // If category exists, increase it's count by 1
+        this.categories.find((category: Category) => {
+          return category.name === task.category;
+        }).count++;
+      } else {
+        // If category doesn't exists, add it and set count to 1
+        this.categories.push({
+          name: task.category,
+          count: 1
+        });
+      }
     });
   }
 }
 
-export type todoItem = {
+export type TodoItem = {
   text: string;
   checked: boolean;
   category: string;
+};
+
+export type Category = {
+  name: string;
+  count: number;
 };
